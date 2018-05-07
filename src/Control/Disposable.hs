f19c16896021c2b5aa5ee42fa352d854f37dba99
Louis Pan <louis@pan.me>
2018-05-07 22:10:38 +1000

    removed Maybe

--- src/Control/Disposable.hs
+++ src/Control/Disposable.hs
@@ -6,22 +6,20 @@ module Control.Disposable
 
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Monad
 import Data.IORef
-import Data.Maybe
 import Data.Semigroup
 import qualified GHCJS.Foreign.Callback as J
 import qualified GHCJS.Foreign.Export as J
 
 -- | A wrapper around authorized IO actions.
-newtype Disposable = Disposable { runDisposable :: Maybe (IO ()) }
+newtype Disposable = Disposable { runDisposable :: IO () }
 
 instance Semigroup Disposable where
-    (Disposable Nothing) <> f = f
-    f <> (Disposable Nothing) = f
-    (Disposable (Just f)) <> (Disposable (Just g)) = Disposable (Just (f >> g))
+    (Disposable f) <> (Disposable g) = Disposable (f >> g)
 
 instance Monoid Disposable where
-    mempty = Disposable Nothing
+    mempty = Disposable (pure ())
     mappend = (<>)
 
 -- | A 'Dispose' is something with some resources to release
@@ -32,27 +30,22 @@ instance Dispose Disposable where
     dispose = id
 
 instance Dispose (J.Callback a) where
-    dispose = Disposable . Just . J.releaseCallback
+    dispose = Disposable . J.releaseCallback
 
 instance Dispose (J.Export a) where
-    dispose = Disposable . Just . J.releaseExport
+    dispose = Disposable . J.releaseExport
 
 instance Dispose a => Dispose (TVar a) where
-    dispose a = Disposable . Just $ do
-        Disposable b <- dispose <$> atomically (readTVar a)
-        fromMaybe mempty b
+    dispose a = Disposable . join $ (runDisposable . dispose) <$> atomically (readTVar a)
 
 instance Dispose a => Dispose (TMVar a) where
-    dispose a = Disposable . Just $ do
-        Disposable b <- dispose <$> atomically (readTMVar a)
-        fromMaybe mempty b
+    dispose a = Disposable . join $
+        (runDisposable . dispose) <$> atomically (readTMVar a)
 
 instance Dispose a => Dispose (IORef a) where
-    dispose a = Disposable . Just $ do
-        Disposable b <- dispose <$> readIORef a
-        fromMaybe mempty b
+    dispose a = Disposable . join $
+        (runDisposable . dispose) <$> readIORef a
 
 instance Dispose a => Dispose (MVar a) where
-    dispose a = Disposable . Just $ do
-        Disposable b <- dispose <$> readMVar a
-        fromMaybe mempty b
+    dispose a = Disposable . join $
+        (runDisposable . dispose) <$> readMVar a
